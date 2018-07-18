15243aacc6f1430e25a9b07dc9b06cc376de1154
tomdebree <tomdebree@hotmail.com>
2018-07-18 20:49:06 +0100

    Scells added

--- TeslaBMSV2.ino
+++ TeslaBMSV2.ino
@@ -17,6 +17,7 @@ EEPROMSettings settings;
 //Simple BMS Settings//
 int CAP = 100; //battery size in Ah
 int Pstrings = 2; // strings in parallel used to divide voltage of pack
+int Scells = 14;//Cells in series
 int ESSmode = 0; //turn on ESS mode, does not respond to key switching
 float storagedelta = 0.3; //in ESS mode in 1 high changes charge and discharge limits by this amount
 
@@ -901,14 +902,14 @@ void VEcan() //communication with Victron system over CAN
 {
   msg.id  = 0x351;
   msg.len = 8;
-  msg.buf[0] = lowByte(uint16_t((settings.ChargeVsetpoint * bms.seriescells() / Pstrings) * 10));
-  msg.buf[1] = highByte(uint16_t((settings.ChargeVsetpoint * bms.seriescells() / Pstrings) * 10));
+  msg.buf[0] = lowByte(uint16_t((settings.ChargeVsetpoint * Scells) * 10));
+  msg.buf[1] = highByte(uint16_t((settings.ChargeVsetpoint * Scells) * 10));
   msg.buf[2] = lowByte(chargecurrent);
   msg.buf[3] = highByte(chargecurrent);
   msg.buf[4] = lowByte(discurrent );
   msg.buf[5] = highByte(discurrent);
-  msg.buf[6] = lowByte(uint16_t((settings.DischVsetpoint * bms.seriescells() / Pstrings) * 10));
-  msg.buf[7] = highByte(uint16_t((settings.DischVsetpoint * bms.seriescells() / Pstrings) * 10));
+  msg.buf[6] = lowByte(uint16_t((settings.DischVsetpoint * Scells) * 10));
+  msg.buf[7] = highByte(uint16_t((settings.DischVsetpoint * Scells) * 10));
   Can0.write(msg);
 
   msg.id  = 0x355;
@@ -1226,6 +1227,9 @@ void menu()
         SERIALCONSOLE.print(Pstrings, 0);
         SERIALCONSOLE.print(" Slave strings in parallel - c");
         SERIALCONSOLE.println("  ");
+        SERIALCONSOLE.print(Scells);
+        SERIALCONSOLE.print(" Cells in series - s");
+        SERIALCONSOLE.println("  ");
         break;
       case 101: //e dispaly settings
         SERIALCONSOLE.println("  ");
@@ -1269,6 +1273,15 @@ void menu()
           Pstrings = Serial.parseInt();
           SERIALCONSOLE.print(Pstrings, 0);
           SERIALCONSOLE.print("Slave strings in parallel");
+        }
+        break;
+
+      case 's': //
+        if (Serial.available() > 0)
+        {
+          Scells = Serial.parseInt();
+          SERIALCONSOLE.print(Scells );
+          SERIALCONSOLE.print(" Cells in series");
         }
         break;
 
